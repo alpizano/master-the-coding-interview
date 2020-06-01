cd4ab7014c1c0f2dc8076b698922ea40a174abd7
alpizano <apizano@purdue.edu>
2020-06-01 05:18:16 -0500

    Adding insert method to BST class and adding Unit Test to build sample BST

--- src/main/java/org/example/BinarySearchTree.java
+++ src/main/java/org/example/BinarySearchTree.java
@@ -1,9 +1,24 @@
 package org.example;
 
 class BinarySearchTree {
-    Node root;
+    Node node;
 
     public void insert(int value) {
+        // Create new Node
+        Node newNode = new Node(value);
+
+        // Case 1: If empty tree
+        if(node == null) {
+            node = newNode;
+        }
+        // Case 2: Value < node.value
+        else if(value < node.value) {
+            insert(value);
+        }
+        // Case 3: Value > node.value
+        else {
+            insert(value);
+        }
 
     }
 }
@@ -12,6 +27,7 @@ class Node {
     int value;
     Node left;
     Node right;
+    private int age;
 
     public Node(int value) {
         this.value=value;
--- src/test/java/org/example/BinarySearchTreeTest.java
+++ src/test/java/org/example/BinarySearchTreeTest.java
@@ -16,8 +16,23 @@ public class BinarySearchTreeTest
     public void setUp() {
         bst = new BinarySearchTree();
     }
+
     @Test
     public void verifyInsert()
     {
+        // Create Tree below:
+        //         9
+        //       /   \
+        //     4      20
+        //   /  \    /  \
+        //  1    6  15  170
+
+        bst.insert(9);
+        bst.insert(4);
+        bst.insert(20);
+        bst.insert(1);
+        bst.insert(6);
+        bst.insert(15);
+        bst.insert(170);
     }
 }
