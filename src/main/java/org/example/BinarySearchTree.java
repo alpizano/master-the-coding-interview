dbe78d3d19e9a4392b014a039c19f5a074b077f5
alpizano <apizano@purdue.edu>
2020-06-01 04:57:38 -0500

    Renamed App.java to BinarySearchTree.java. Adding Unit Test to BinarySearchTreeTest.java for verifying insert method

---
+++ src/main/java/org/example/BinarySearchTree.java
@@ -0,0 +1,21 @@
+package org.example;
+
+class BinarySearchTree {
+    Node root;
+
+    public void insert(int value) {
+
+    }
+}
+
+class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value) {
+        this.value=value;
+        this.left = null;
+        this.right = null;
+    }
+}
---
+++ src/test/java/org/example/BinarySearchTreeTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class BinarySearchTreeTest
+{
+    BinarySearchTree bst;
+
+    @Before
+    public void setUp() {
+        bst = new BinarySearchTree();
+    }
+    @Test
+    public void verifyInsert()
+    {
+    }
+}
