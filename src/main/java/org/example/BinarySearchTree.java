499aa0ad4b92bea03be32f4d6e2594ccc36eca16
alpizano <apizano@purdue.edu>
2020-06-01 06:03:48 -0500

    Fixed bug with insert method. Successfully creates BST, not using inner class

--- src/main/java/org/example/BinarySearchTree.java
+++ src/main/java/org/example/BinarySearchTree.java
@@ -1,28 +1,30 @@
 package org.example;
 
 class BinarySearchTree {
-    Node node;
+    Node root;
 
     public void insert(int value) {
-        insert(node, value);
+        root = insert(root, value);
     }
 
-    public void insert(Node node, int value) {
+    public Node insert(Node node, int value) {
         // Create new Node
         Node newNode = new Node(value);
 
         // Case 1: If empty tree
         if(node == null) {
-            node = newNode;
+            //node = newNode;
+            return newNode;
         }
         // Case 2: Value < node.value
         else if(value < node.value) {
-            insert(node.left, value);
+            node.left =insert(node.left, value);
         }
         // Case 3: Value > node.value
         else {
-            insert(node.right, value);
+            node.right = insert(node.right, value);
         }
+        return node;
     }
 }
 
@@ -30,7 +32,6 @@ class Node {
     int value;
     Node left;
     Node right;
-    private int age;
 
     public Node(int value) {
         this.value=value;
